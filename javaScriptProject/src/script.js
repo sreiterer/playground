a2d683aa1e0e32d3a3224a072158452566459d10
Stefan Reiterer <stefan.reiterer@inso.tuwien.ac.at>
2013-01-10 18:19:57 +0100

    Initial Commit with all testproject templates

---
+++ javaScriptProject/src/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: stefan
+ * Date: 08.01.13
+ * Time: 20:35
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function() {
+    setText();
+});
+
+function setText() {
+    $("#entryPoint").text("das ist ein test");
+}
---
+++ javaScriptProject/test/spec/spec.js
@@ -0,0 +1,6 @@
+describe("Test", function() {
+
+  it("should write something into a div element", function() {
+    expect("hello").toBe("hello");
+  });
+});
